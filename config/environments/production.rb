c0d5dd4fde1598c84f8079f221aad6458a3e9cfb
Jesse Wolgamott <jesse.wolgamott@gmail.com>
2012-04-30 16:47:00 -0500

    Fix http path prob

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,5 +64,5 @@ Rubyfiddle::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.ruby_fiddle_exec_url = "http://ruby-fiddle-cedar.herokuapp.com"
+  config.ruby_fiddle_exec_url = "http://ruby-fiddle-cedar.herokuapp.com/"
 end
