4879801141c167893d5542c01e444193c6ae508f
Jesse Wolgamott <jesse@comalproductions.com>
2014-09-22 17:18:26 -0500

    OK, so make the twitter link work. #fatfingurrd

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rubyfiddle::Application.routes.draw do
     end
   end
   get 'riddles/:id/:version' => 'riddles#show', :as => :versioned_riddle, :constraints => { :version=> /\d*/}
+  get 'riddles/c07a2m' => redirect("/riddles/c07a2")
   post 'plays/run' => "plays#run", as: :play
 end
