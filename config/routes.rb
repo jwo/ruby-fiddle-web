dd299443aeaac3d1a1aa2a44283ea531004b4a64
Jesse Wolgamott <jesse@comalproductions.com>
2014-09-23 08:10:56 -0500

    switch those priorities around

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rubyfiddle::Application.routes.draw do
       put :fork
     end
   end
-  get 'riddles/:id/:version' => 'riddles#show', :as => :versioned_riddle, :constraints => { :version=> /\d*/}
   get 'riddles/c07a2m' => redirect("/riddles/c07a2")
+  get 'riddles/:id/:version' => 'riddles#show', :as => :versioned_riddle, :constraints => { :version=> /\d*/}
   post 'plays/run' => "plays#run", as: :play
 end
