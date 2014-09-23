c11b31e6d19faf72c85a82e72dfd812d77009597
Jesse Wolgamott <jesse@comalproductions.com>
2014-09-23 09:26:34 -0500

    one more sweet time

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rubyfiddle::Application.routes.draw do
   root to: "riddles#index"
+  get 'riddles/c07a2m' => redirect("/riddles/c07a2")
   resources :riddles do
     member do
       put :fork
     end
   end
-  get 'riddles/c07a2m' => redirect("/riddles/c07a2")
   get 'riddles/:id/:version' => 'riddles#show', :as => :versioned_riddle, :constraints => { :version=> /\d*/}
   post 'plays/run' => "plays#run", as: :play
 end
